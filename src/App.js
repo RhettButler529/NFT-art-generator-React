3af4ab181e3baf79560cf9ef80eb24c4273597f2
Rhett <rhettbutler0529@gmail.com>
2022-11-14 23:46:04 +0900

    tmp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home';
 import About from './pages/about';
+import Generator from './pages/generator';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path = "/" element = {<Navigate to = "/home"/>} />
         <Route path = "/home" element = {<Home />} />
         <Route path = "/about" element = {<About />} />
+        <Route path = "/generator" element = {<Generator />} />
       </Routes>
     </div>
   );
---
+++ src/pages/generator/index.jsx
@@ -0,0 +1,25 @@
+import BottomNav from "../../components/common/BottomNav";
+import Layout from "../../components/common/Layout";
+// import Data from "../../components/Data";
+// import Layers from "../../components/Layers";
+import React from 'react'
+import styles from "./index.module.scss";
+
+
+function Generator() {
+  return (
+    <nav>
+      <p>HGeome</p>
+    </nav>
+    // <Layout dullBackground>
+    //   <div className={styles.container}>
+    //     <Data />
+    //     <Layers />
+    //   </div>
+
+    //   <BottomNav />
+    // </Layout>
+  );
+}
+
+export default Generator;
